0f5f022ae73f8c39015362cf928550e9c8edcb8e
Cristobal Cenalmor <cr2.cristobal@gmail.com>
2020-05-30 11:10:28 +0200

    DONE: Demo Data, Models, Manifest, ICON, ER

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "final/",
+    'name': "final",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Aplicacion final desarrollada por Cristobal
+       Cenalmor para el modulo de ERPs""",
 
     'description': """
-        Long description of module's purpose
+        Aplicacion final para ERPs que administrara y 
+        gestionara informacion relativa a libros, autores
+        y editoriales y sus relaciones. 
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "cristobalcp",
+    'website': "https://github.com/cristobalcp/final",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,10 +26,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/final.xml',
+        #'views/reports.xml',
     ],
+    'application':'True',
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
--- models/models.py
+++ models/models.py
@@ -1,18 +1,53 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+
+# Modelo Libro   
+class Libro(models.Model):
+    _name = 'final.libro'
+    _description = "Final Libro"
+
+    name = fields.Char(string="Titulo", required=True)
+    datetime = fields.Datetime(string='Dia Lanzamiento', required=True)    
+    pages = fields.Integer(string='Paginas')    
+    price = fields.Float(string="Precio (€)")
+    sinopsis = fields.Char(string="Sinopsis")
+    isbn = fields.Char(string="Isbn")
+
+    # Lo relacionamos con el modelo Autor
+    autor_id = fields.Many2one('final.autor',
+        ondelete='cascade', string="Autor", required=True, default=lambda self: self.env.user)
+    
+    # Lo relacionamos con el modelo Editorial
+    editorial_id = fields.Many2one('final.editorial',
+        string="Editorial", required=True, default=lambda self: self.env.user)
+
+# Modelo Autor   
+class Autor(models.Model):
+    _name = 'final.autor'
+    _description = "Final Autor"
+
+    name = fields.Char(string="Nombre", required=True)
+    nationality = fields.Char(string="Nacionalidad")
+    year = fields.Integer(string="Año")
+    sex = fields.Char(string="Sexo")
+
+    # Lo relacionamos con el modelo Libro
+    libro_id = fields.One2many('final.libro',
+        'autor_id', string="Libros", default=lambda self: self.env.user)
+
+# Modelo Editorial   
+class Editorial(models.Model):
+    _name = 'final.editorial'
+    _description = "Final Editorial"
+
+    name = fields.Char(string="Nombre", required=True)
+    address = fields.Char(string="Direccion")
+    email = fields.Char(string="Email")
+    phone = fields.Char(string="Telefono")
+
+    # Lo relacionamos con el modelo Libro
+    # libro_id = fields.One2many('final.libro',
+    #     'editorial_id', string="Libros", default=lambda self: self.env.user)
 
 
-# class final/(models.Model):
-#     _name = 'final/.final/'
-#     _description = 'final/.final/'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
